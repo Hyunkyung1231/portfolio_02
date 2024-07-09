6c3b940b0dcac5a9d18478d8ea56add62544ee75
Hyunkyung1231 <gusrud4337@gmail.com>
2024-07-09 11:09:33 +0900

    delete gothic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,10 @@
 import "@/styles/globals.css";
 import Navbar from "@/components/Navbar/Navbar";
 
-import { Nanum_Gothic } from "next/font/google";
-export const gothic = Nanum_Gothic({
-  weight: ["400", "700", "800"],
-  subsets: ["latin"],
-});
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }) {
   return (
     <html lang="ko" className="no-scrollbar box-border h-full w-full">
-      <body
-        className={`${gothic.className} no-scrollbar bg-primary-20 relative m-auto box-border flex h-full flex-col items-center`}
-      >
+      <body className="no-scrollbar relative m-auto box-border flex h-full flex-col items-center bg-primary-20">
         <div className="fixed top-0 z-50 w-full">
           <Navbar />
         </div>
