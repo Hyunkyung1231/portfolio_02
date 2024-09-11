47557352ed600b42391b0e0ed3a7264f8575bfea
Hyunkyung1231 <gusrud4337@gmail.com>
2024-09-11 17:52:34 +0900

    add slide and mordal

---
+++ components/DashedLine/DashedLine.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { RxBorderDashed } from "react-icons/rx";
+
+const DashedLine = () => {
+  return (
+    <>
+      <RxBorderDashed size={20} />
+      <RxBorderDashed size={20} />
+      <RxBorderDashed size={20} />
+      <RxBorderDashed size={20} />
+      <RxBorderDashed size={20} />
+      <RxBorderDashed size={20} />
+    </>
+  );
+};
+
+export default DashedLine;
